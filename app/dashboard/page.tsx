4c93b28d577a20a33f8a6459be02a5dd391c49dc
jai-bharghav <128003096@sastra.ac.in>
2025-12-29 10:46:57 +0530

    dashboard page added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,9 +6,6 @@ export default function DashboardPage() {
       <p className="mt-4 italic text-gray-700 dark:text-gray-400">
         This is your dashboard.
       </p>
-      <p className="mt-4 italic text-gray-700 dark:text-gray-400">
-        TEXT COLOR TEST
-      </p>
     </main>
   );
 }
